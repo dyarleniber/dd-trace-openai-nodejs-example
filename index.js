82e4559e98c2fe4cbdd316b002fd6b8859ac56df
Dyarlen Iber <dyarlen1@gmail.com>
2024-05-02 15:32:26 +0200

    Remove Typescript

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
 import dataDogTracer from "dd-trace";
 import express from "express";
 import OpenAI from "openai";
+
 dataDogTracer.init({
-    logInjection: true,
-    runtimeMetrics: true,
+  logInjection: true,
+  runtimeMetrics: true,
 });
+
 const openAIClient = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.OPENAI_API_KEY,
 });
+
 const app = express();
+
 app.get("/", async (req, res) => {
-    const response = await openAIClient.chat.completions.create({
-        messages: [{ role: "user", content: "How old is the earth?" }],
-        model: "gpt-4-turbo",
-        temperature: 0,
-    });
-    res.status(200).json({ response: response.choices?.[0]?.message?.content });
+  const response = await openAIClient.chat.completions.create({
+    messages: [{ role: "user", content: "How old is the earth?" }],
+    model: "gpt-4-turbo",
+    temperature: 0,
+  });
+  res.status(200).json({ response: response.choices?.[0]?.message?.content });
 });
+
 app.listen(3000, () => {
-    console.log("server listening on port 3000");
+  console.log("server listening on port 3000");
 });
